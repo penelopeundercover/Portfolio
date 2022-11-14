5b1c4169abaedf65cc0057bb7426e5b611a5441f
penelopeundercover <mjseiwert@outlook.com>
2022-11-14 11:49:58 -0600

    monday

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
+import background from "../background.jpg";
 
 function Header(props) {
   const [shown, setShown] = useState(false);
 
   return (
     <header className="App-header">
-      <h1>Marianne Seiwert</h1>
-      <nav>
-        {/* TODO: Make header fixed. Refer to sticky tab. */}
-        <a className="smoothscroll" href="#about">
-          About
-        </a>
-        <a className="smoothscroll" href="#projects">
-          Projects
-        </a>
+      <div style={{ backgroundImage: `url(${background})` }}>
+        <h1>Marianne Seiwert</h1>
+        <nav>
+          {/* TODO: Make header fixed. Refer to sticky tab. */}
+          <a className="smoothscroll" href="#about">
+            About
+          </a>
+          <a className="smoothscroll" href="#projects">
+            Projects
+          </a>
 
-        {/* TODO: Set up the pdf viewer for resume. Use resume.js or data.js or however it should be. https://react-pdf-viewer.dev/docs/basic-usage/*/}
-        {/* https://react-pdf-viewer.dev/examples/preview-a-document-inside-a-modal/ */}
-        <button onClick={() => setShown(true)}>Resume</button>
+          {/* TODO: Set up the pdf viewer for resume. Use resume.js or data.js or however it should be. https://react-pdf-viewer.dev/docs/basic-usage/*/}
+          {/* https://react-pdf-viewer.dev/examples/preview-a-document-inside-a-modal/ */}
+          <button onClick={() => setShown(true)}>Resume</button>
 
-        <a className="smoothscroll" href="#contact">
-          Contact
-        </a>
-      </nav>
+          <a className="smoothscroll" href="#contact">
+            Contact
+          </a>
+        </nav>
+      </div>
     </header>
   );
 }
