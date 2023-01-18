c1bdc0d09d379832e82babbdd7aa4babeb2dc244
penelopeundercover <mjseiwert@outlook.com>
2023-01-18 15:49:09 -0600

    made background image and header for all pages, fixed broken image link

--- src/components/About.js
+++ src/components/About.js
@@ -1,46 +1,35 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Header from "./Header";
+import MarianneSeiwert from "./MarianneSeiwert.jpg";
 
 function About() {
   return (
-    <div id="about">
-      <div id="header">
-        <h1>Marianne Seiwert</h1>
-        <ul>
-          <li>
-            <Link to="/Portfolio/">About</Link>
-          </li>
-          <li>
-            <Link to="/Portfolio/projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/Portfolio/resume">Resume</Link>
-          </li>
-          <li>
-            <Link to="/Portfolio/contact">Contact</Link>
-          </li>
-        </ul>
+    <>
+      <Header></Header>
+      <div id="about">
+        <section id="about-me">
+          <h1>About Me</h1>
+          <div id="bio">
+            <img
+              src={MarianneSeiwert}
+              alt="Marianne"
+              className="Marianne"
+            ></img>
+            <p>
+              Straight-shooting and intuitive full stack web developer ready to
+              use MERN Stack skills in a lively collaborative environment.
+              Recently earned a certificate from the University of Kansas Coding
+              Bootcamp while working full-time as a bulldozer operator.
+              Construction experience has developed tenacity, solid work ethic,
+              and a make it work mindset. Seeking a long-term position in a
+              strong company that expects high-quality output and positive
+              collaboration.
+            </p>
+          </div>
+        </section>
       </div>
-      <section id="about-me">
-        <h1>About Me</h1>
-        <div id="bio">
-          <img
-            src="MarianneSeiwert.jpg"
-            alt="Marianne"
-            className="Marianne"
-          ></img>
-          <p>
-            Straight-shooting and intuitive full stack web developer ready to
-            use MERN Stack skills in a lively collaborative environment.
-            Recently earned a certificate from the University of Kansas Coding
-            Bootcamp while working full-time as a bulldozer operator.
-            Construction experience has developed tenacity, solid work ethic,
-            and a make it work mindset. Seeking a long-term position in a strong
-            company that expects high-quality output and positive collaboration.
-          </p>
-        </div>
-      </section>
-    </div>
+    </>
   );
 }
 
