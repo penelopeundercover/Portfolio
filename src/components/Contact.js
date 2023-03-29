5536e032149ecb514839b26d11f7e41e8ac02fb8
penelopeundercover <mjseiwert@outlook.com>
2023-03-28 21:19:56 -0500

    contact nearly finished

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../styling/Contact.css";
 import { FaGithub } from "react-icons/fa";
 import { SiLinkedin } from "react-icons/si";
+import { HiOutlineMail } from "react-icons/hi";
+import { IconContext } from "react-icons";
 
 function Contact() {
   return (
@@ -11,21 +13,64 @@ function Contact() {
           <h1>Let's Connect!</h1>
           <div className="contact-info">
             <ul>
-              <li>mjseiwert@outlook.com</li>
               <li>
-                <a href="https://github.com/penelopeundercover" target="blank">
-                  <FaGithub />
-                </a>
-                https://www.linkedin.com/in/marianne-seiwert/
+                <button
+                  onClick={() => {
+                    navigator.clipboard.writeText("mjseiwert@outlook.com");
+                  }}
+                >
+                  <IconContext.Provider
+                    value={{
+                      color: "rgb(234, 4, 126)",
+                      size: "2.25em",
+                    }}
+                  >
+                    <HiOutlineMail className="email" />
+                  </IconContext.Provider>
+                </button>
+                <button
+                  onClick={() => {
+                    navigator.clipboard.writeText("mjseiwert@outlook.com");
+                  }}
+                >
+                  mjseiwert@outlook.com
+                </button>
               </li>
+
+              <li>
+                <a
+                  href="https://github.com/penelopeundercover"
+                  target="blank"
+                  class="github"
+                >
+                  <IconContext.Provider
+                    value={{
+                      color: "rgb(56, 229, 77)",
+                      size: "2em",
+                    }}
+                  >
+                    <FaGithub className="github" />
+                  </IconContext.Provider>
+                </a>
+                https://github.com/penelopeundercover
+              </li>
+
               <li>
                 <a
                   href="https://www.linkedin.com/in/marianne-seiwert"
                   target="blank"
+                  class="linkedin"
                 >
-                  <SiLinkedin />
+                  <IconContext.Provider
+                    value={{
+                      color: "rgb(33, 146, 255)",
+                      size: "2em",
+                    }}
+                  >
+                    <SiLinkedin className="linkedin" />
+                  </IconContext.Provider>
                 </a>
-                https://github.com/penelopeundercover
+                https://www.linkedin.com/in/marianne-seiwert
               </li>
             </ul>
           </div>
