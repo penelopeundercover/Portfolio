9d2cdb87ae79903f2b110979645f3e22e51d56e0
penelopeundercover <mjseiwert@outlook.com>
2023-04-25 20:14:26 -0500

    pdf viewer in progress

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,29 +1,30 @@
 // Future: Use react pdf viewer for resume.
 import React, { useState } from "react";
-import { Document, Page } from "react-pdf/dist/exm/entry.webpack";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
 import "../styling/Resume.css";
 
-const Resume = () => {
+function Resume() {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
-  const onDocumentLoadSuccess = ({ numPages }) => {
+  function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
-  };
+    setPageNumber(1);
+  }
 
-  const goToPrevPage = () =>
-    setPageNumber(pageNumber - 1 <= 1 ? 1 : pageNumber - 1);
-
-  const goToNextPage = () =>
-    setPageNumber(pageNumber + 1 >= numPages ? numPages : pageNumber + 1);
-  
   return (
     <div>
-      <nav>
- 
-      </nav>
-  </div>>
+      <Document
+        file="./Marianne-Seiwert-Resume-ATS.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
   );
-};
+}
 
 export default Resume;
