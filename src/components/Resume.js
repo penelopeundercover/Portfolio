16378097135ae7515039596e943cf873641e3501
penelopeundercover <mjseiwert@outlook.com>
2023-12-03 17:56:05 -0600

    fixed resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,37 +1,15 @@
-//TODO: Fix annotation issues.
-// TODO: Make it downloadable and printable.
-import React, { useState, useEffect } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+//TODO: Make iframe responsive
+
+import React from "react";
 import "../styling/Resume.css";
 
 function Resume() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-    setPageNumber(1);
-  }
-  useEffect(() => {
-    pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-  });
-
   return (
     <div>
-      <Document
-        file={process.env.PUBLIC_URL + "/Marianne-Seiwert-Resume-ATS.pdf"}
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        <Page
-          className="resumePage"
-          pageNumber={1}
-          renderAnnotationLayer={true}
-          externalLinkTarget="_blank"
-          renderTextLayer={false}
-        />
-      </Document>
-      <p>Page {pageNumber}</p>
+      <iframe
+        src={process.env.PUBLIC_URL + "/Marianne-Seiwert-Resume-ATS.pdf"}
+        title="Marianne Seiwert Resume"
+      ></iframe>
     </div>
   );
 }
