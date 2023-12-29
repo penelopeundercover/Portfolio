20be3d8a5ad933133715bb04598dcb077b075044
penelopeundercover <mjseiwert@outlook.com>
2023-12-29 15:49:05 -0600

    updated about me

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,16 @@
-// TODO: Make box appear with fade effect on page load
-//       Stop fade effect after initial visit to the homepage
+// TODO: Make box appear with fade effect on page load without having to click.
+//       Stop fade effect after initial visit to the homepage.
 
 import React from "react";
 import "../styling/Home.css";
 import MarianneSeiwert from "./MarianneSeiwert.jpg";
-import { loadFromFile } from "react-pdf/dist/cjs/shared/utils";
 
 function Home() {
   return (
     <>
       <div className="about">
         <section className="about-me">
-          <h1>About Me</h1>
+          <h1>Hi! I'm Marianne.</h1>
           <div className="bio">
             <img
               src={MarianneSeiwert}
@@ -19,6 +18,15 @@ function Home() {
               className="Marianne"
             ></img>
             <p>
+              I recently earned a certificate from the University of Kansas
+              Coding Bootcamp while working full-time as a bulldozer operator.
+              Currently continuing to grow as a full stack web developer with
+              skills in the MERN stack. I'm especially passionate about working
+              with databases. In addition to coding and bulldozers, I love
+              playing the piano, cooking, nurturing my beloved houseplants, and
+              reading.
+            </p>
+            {/* <p>
               Straight-shooting and intuitive full stack web developer ready to
               use MERN Stack skills in a lively collaborative environment.
               Recently earned a certificate from the University of Kansas Coding
@@ -27,7 +35,7 @@ function Home() {
               and a make-it-work mindset. Seeking a long-term position in a
               strong company that expects high-quality output and positive
               collaboration.
-            </p>
+            </p> */}
           </div>
         </section>
       </div>
