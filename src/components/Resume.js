77b73ee2ae675eff23d2fb5b2e1dee34b2a8e605
penelopeundercover <mjseiwert@outlook.com>
2024-03-04 20:12:50 -0600

    changed resume back to image

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "../styling/Resume.css";
+import jpgResume from "./jpgResume.jpg";
 
 function Resume() {
   return (
     <div class="resume-page">
-      <iframe
+      <img src={jpgResume} alt="Marianne's Resume" className="resume"></img>
+      {/* <iframe
         src={process.env.PUBLIC_URL + "/Marianne-Seiwert-Resume-ATS.pdf"}
         title="Marianne Seiwert Resume"
-      ></iframe>
+      ></iframe> */}
     </div>
   );
 }
